9be8d0d31435e9e40e0ed0bb301f8a3702c48d2a
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-07-01 16:32:40 +0100

    Refactored tests to use 'subject'

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 require 'webmock/rspec'
 
 describe Transaction do
+  let(:subject) { Transaction.new }
   describe '#geteuros' do
     it 'makes successful call to API' do
       uri = URI('https://openexchangerates.org/')
@@ -12,10 +13,9 @@ describe Transaction do
   end
   describe '#calculate' do
     it 'calculates how much cheese the customer can buy with £19.99' do
-      transaction = Transaction.new
       WebMock.allow_net_connect!
-      allow(transaction).to receive(:geteuros) { 2 }
-      expect(transaction.calculate).to eq 12
+      allow(subject).to receive(:geteuros) { 2 }
+      expect(subject.calculate).to eq 12
     end
   end
 end
