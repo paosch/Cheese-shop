d5a0fafebeae1a7788e62afb16bf1a27f365fa57
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-07-01 16:26:19 +0100

    The #calculate method returns the amount of cheese a customer can buy

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -3,13 +3,16 @@ require 'rubygems'
 require 'httparty'
 
 class Transaction
-  attr_accessor :euros, :response
+  attr_accessor :euros
   include HTTParty
   base_uri 'https://openexchangerates.org/api'
 
   def geteuros
     euros = self.class.get('/convert/1/GBP/EUR?app_id=8567d4e0f026487db09bafbfbfda2069')
-    puts euros
     euros['response']
+  end
+
+  def calculate
+    ((geteuros * 19.99) / 3.24).to_i
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -10,4 +10,12 @@ describe Transaction do
       expect(response).to be_an_instance_of(String)
     end
   end
+  describe '#calculate' do
+    it 'calculates how much cheese the customer can buy with £19.99' do
+      transaction = Transaction.new
+      WebMock.allow_net_connect!
+      allow(transaction).to receive(:geteuros) { 2 }
+      expect(transaction.calculate).to eq 12
+    end
+  end
 end
