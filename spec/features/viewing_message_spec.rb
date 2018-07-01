5568a975ed65bb65e197b2b4e86062b44e35e203
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-07-01 18:25:15 +0100

    allowed real connections in feature test

--- spec/features/viewing_message_spec.rb
+++ spec/features/viewing_message_spec.rb
@@ -1,5 +1,6 @@
 feature 'Confirmation message for customers' do
   scenario 'the customer can see how many blocks of cheese they get' do
+    WebMock.allow_net_connect!
     visit('/')
     expect(page).to have_content 'Today you can buy'
   end
