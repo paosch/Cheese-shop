dc1d39669ba1e2ee88304b96cfd17e3c9124984d
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-07-04 11:15:08 +0100

    Flags appear on the homepage

--- app.rb
+++ app.rb
@@ -2,9 +2,7 @@ require 'sinatra'
 require_relative './lib/transaction.rb'
 class CheeseEmporium < Sinatra::Base
   get '/' do
-    transaction = Transaction.new
-    @final = transaction.calculate
-    erb :index
+    erb :index1
   end
   run! if app_file == $0
 end
--- spec/features/viewing_message_spec.rb
+++ spec/features/viewing_message_spec.rb
@@ -1,7 +1,12 @@
 feature 'Confirmation message for customers' do
-  scenario 'the customer can see how many blocks of cheese they get' do
+  # scenario 'the customer can see how many blocks of cheese they get' do
+  #   WebMock.allow_net_connect!
+  #   visit('/')
+  #   expect(page).to have_content 'Today you can buy'
+  # end
+  scenario 'the page shows two flags' do
     WebMock.allow_net_connect!
     visit('/')
-    expect(page).to have_content 'Today you can buy'
+    expect(page).to have_content 'in France'
   end
 end
